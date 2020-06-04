c47c0ae5912573ef6e168a7a9e03842e157bf209
megan-pg <megan.pardygokcu@unh.edu>
2020-06-04 18:47:43 -0400

    created employeeclass

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,9 @@
 // TODO: Write code to define and export the Employee class
+class Employee {
+    constructor(name, title, id, email) {
+        this.name = name;
+        this.title = title;
+        this.id = id;
+        this.email = email;
+    }
+}
