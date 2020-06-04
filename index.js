a9c4bb55ad271ca3307c38a9ebf9af9b52b793c3
megan-pg <megan.pardygokcu@unh.edu>
2020-06-04 18:42:37 -0400

    added employee funtion

---
+++ index.js
@@ -0,0 +1,9 @@
+function Employee(name, id, email) {
+    this.name = name
+    this.id = id
+    this.email = email
+};
+
+
+const megan = new Employee("Megan", "55555", "meg.pardy@gmail.com");
+const joe = new Employee("Joe", "12345", "joesmith@gmail.com");
