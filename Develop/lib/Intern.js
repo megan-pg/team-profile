205bccfdfcc3c214c1e27cff8084eb8c63d3e973
megan-pg <megan.pardygokcu@unh.edu>
2020-06-10 16:27:26 -0400

    created manager and intern classes

--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,1 +1,20 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee.js")
+
+class Intern extends Employee {
+    constructor(name, title, id, email, github) {
+        super(name, title, id, email);
+        this.github = github;
+
+    }
+    getGithub() {
+        return github;
+    }
+    getRole() {
+        return "Intern";
+    }
+
+}
+
+
+module.exports = Intern;
--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,1 +1,20 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+const Employee = require("./Employee.js")
+
+class Manager extends Employee {
+    constructor(name, title, id, email, github) {
+        super(name, title, id, email);
+        this.github = github;
+
+    }
+    getGithub() {
+        return github;
+    }
+    getRole() {
+        return "Manager";
+    }
+
+}
+
+
+module.exports = Manager;
